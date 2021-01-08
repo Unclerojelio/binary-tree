aa91685675ab289924041a8fb8797a2246c2ce7d
Roger Banks <roger_banks@mac.com>
2021-01-07 23:46:56 -0600

    implement insert

--- btree.py
+++ btree.py
@@ -3,10 +3,28 @@ from node import Node
 
 class BinaryTree(object):
     def __init__(self):
-        self.root = Node()
-    def insert(self, value):
-        newNode = Node(value)
-        self.root.insert(newNode)
+        self.root = None
+    def insert(self, value, currNode=None):
+        if self.root == None and currNode == None:
+            currNode = Node(value)
+            self.root = currNode
+        elif currNode == None:
+            self.insert(value, self.root)
+        elif currNode.value == value:
+            currNode.incrementCount()
+        elif currNode.value > value:
+            if currNode.getLeftChild() == None:
+                newNode = Node(value)
+                currNode.addLeftChild(newNode)
+            else:
+                self.insert(value, currNode.getLeftChild())
+        elif currNode.value < value:
+            if currNode.getRightChild() == None:
+                newNode = Node(value)
+                currNode.addRightChild(newNode)
+            else:
+                self.insert(value, currNode.getRightChild())
+
     def printPreorder(self):
         self.root.printPreorder()
     def printInorder(self):
@@ -19,3 +37,6 @@ class BinaryTree(object):
         return self.root.isBalanced()
     def find(self, value):
         return self.root.find(value)
+
+    def __repr__(self):
+        return repr(self.root)
--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from random import randint
 from btree import BinaryTree
 
 def main():
+    # myTree = BinaryTree()
+    # myTree.printInorder()
+    # for _ in range(1000):
+    #     value = randint(0,20)
+    #     myTree.insert(value)
+    # print("height: ", myTree.height(), myTree.root.left.height(), myTree.root.right.height())
+    # print(myTree.isBalanced())
+    # myTree.printInorder()
+    # myTree.find(12)
+    # myTree.find(200)
+
     myTree = BinaryTree()
-    myTree.printInorder()
-    for _ in range(1000):
-        value = randint(0,20)
-        myTree.insert(value)
-    print("height: ", myTree.height(), myTree.root.left.height(), myTree.root.right.height())
-    print(myTree.isBalanced())
-    myTree.printInorder()
-    myTree.find(12)
-    myTree.find(200)
+    myTree.insert(2)
+    myTree.insert(3)
+    myTree.insert(1)
+    print(myTree)
 
 if __name__ == "__main__":
     main()
