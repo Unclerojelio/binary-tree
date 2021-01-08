f440ad30c86ca0b7c3c1f5a149dce92bc9a57d6e
Roger Banks <roger_banks@mac.com>
2021-01-07 21:01:23 -0600

    Add tests for Node.

---
+++ node_test.py
@@ -0,0 +1,47 @@
+import unittest
+from node import Node
+
+class TestNode(unittest.TestCase):
+
+    def testInit(self):
+        n = Node()
+        self.assertIsNone(n.getValue())
+        self.assertEqual(n.getCount(), 0)
+        self.assertIsNone(n.getLeftChild())
+        self.assertIsNone(n.getRightChild())
+        n = Node(1)
+        self.assertEqual(n.getValue(), 1)
+
+    def testSetValue(self):
+        n = Node()
+        n.setValue(1)
+        self.assertEqual(n.getValue(), 1)
+
+    def testGetValue(self):
+        n = Node(2)
+        self.assertEqual(n.getValue(), 2)
+
+    def testIncrementCount(self):
+        n = Node()
+        n.incrementCount()
+        self.assertEqual(n.getCount(), 1)
+
+    def testGetLeftChild(self):
+        n = Node()
+        self.assertIsNone(n.getLeftChild())
+
+    def testGetRightChild(self):
+        n = Node()
+        self.assertIsNone(n.getRightChild())
+
+    def testAddLeftChild(self):
+        n = Node()
+        m = Node()
+        n.addLeftChild(m)
+        self.assertIsNotNone(n.getLeftChild())
+
+    def testAddRightChild(self):
+        n = Node()
+        m = Node()
+        n.addRightChild(m)
+        self.assertIsNotNone(n.getRightChild())
